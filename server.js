046b323a5c0a8510d302eea960a7c37131e70596
Najua Saad <najuasaad@gmail.com>
2021-05-06 10:08:38 -0500

    mongo atlas

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+// const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -15,10 +16,14 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budgetDB", config);
+
+const config = { 
+  useNewUrlParser: true, 
+  useUnifiedTopology: true, 
+  //useCreateIndex: true, 
+  //useFindAndModify: false 
+}
 
 // routes
 app.use(require("./routes/api.js"));
